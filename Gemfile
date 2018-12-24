75180a9721ad0e71c4c57a7d3718142aac9987f5
Alex <info@ezwebsupport.com>
2018-12-24 04:41:15 +0000

    api created

--- Gemfile
+++ Gemfile
@@ -15,11 +15,18 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'devise'
 gem 'twitter-bootstrap-rails'
 gem 'devise-bootstrap-views'
+gem 'doorkeeper'
+
+gem 'webpacker'
+gem 'react-rails'
+# gem 'react_on_rails'
+
+gem 'responders'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+# gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,25 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
-  
-  # after_filter :flash_to_headers
+  protect_from_forgery with: :null_session
 
-  # def flash_to_headers
-  #   return unless request.xhr?
-  #   response.headers['X-Message'] = flash_message
-  #   response.headers["X-Message-Type"] = flash_type.to_s
+  # Devise code
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :doorkeeper_authorize!
+  respond_to :json
+
+  protected
+
+  # Devise methods
+  # Authentication key(:username) and password field will be added automatically by devise.
+  def configure_permitted_parameters
+    added_attrs = [:email, :first_name, :last_name]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
   
-  #   flash.discard # don't want the flash to appear when you reload page
-  # end
-  
-  # private
-  
-  # def flash_message
-  #   [:error, :warning, :notice, nil].each do |type|
-  #     return "" if type.nil?
-  #     return flash[type] unless flash[type].blank?
-  #   end
-  # end
-  
-  # def flash_type
-  #   [:error, :warning, :notice, nil].each do |type|
-  #       return "" if type.nil?
-  #       return type unless flash[type].blank?
-  #   end
-  # end
+  private
+
+  # Doorkeeper methods
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 end
